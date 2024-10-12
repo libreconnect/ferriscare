cf4fd627be1091c249f427cc068378d891f8bad6
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-10-12 04:29:06 +0200

    feat: Implement HTTP server with router and listener
    This commit adds the `HttpServer` struct with its associated methods and dependencies. It includes a router for handling API routes and a TCP listener for accepting incoming connections. The `new` method initializes the server with a given configuration, and the `run` method starts the server and listens for incoming requests.
    
    The purpose of this commit is to implement the HTTP server functionality, allowing the application to handle API requests.
    
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -1,19 +1,32 @@
 use std::sync::Arc;
 
 use anyhow::Ok;
-use ferriscare::{domain::professional::ports::ProfessionalRepository, infrastructure::{db::neo4j::Neo4j, professional::neo4j::professional_repository::Neo4jProfessionalRepository}};
+use ferriscare::{
+    application::http::{HttpServer, HttpServerConfig},
+    infrastructure::{
+        db::neo4j::Neo4j, professional::neo4j::professional_repository::Neo4jProfessionalRepository,
+    },
+};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    println!("Hello, world!");
-    
+    dotenv::dotenv().ok();
+    tracing_subscriber::fmt::init();
+
     let database = Neo4j::new("0.0.0.0:7687", "neo4j", "password").await;
     let neo4j = Arc::new(database);
 
-    let professional_repository = Neo4jProfessionalRepository::new(Arc::clone(&neo4j));
+    let server_config = HttpServerConfig { port: "3333" };
 
-    let _ = professional_repository.create("Nathael", "nathael@bonnal.cloud").await;
-    let _ = professional_repository.create("Martin", "martin@matin.io").await;
+    // let professional_repository = Neo4jProfessionalRepository::new(Arc::clone(&neo4j));
 
-    Ok(())
+    // let _ = professional_repository
+    //     .create("Nathael", "nathael@bonnal.cloud")
+    //     .await;
+    // let _ = professional_repository
+    //     .create("Martin", "martin@matin.io")
+    //     .await;
+
+    let http_server = HttpServer::new(server_config).await?;
+    http_server.run().await
 }
