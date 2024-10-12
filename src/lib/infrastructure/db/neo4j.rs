8e23ff8cc6b362e3bc8b05288e67972b248f67e1
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-10-12 02:47:04 +0200

    feat: Add Neo4j infrastructure module
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

---
+++ src/lib/infrastructure/db/neo4j.rs
@@ -0,0 +1,21 @@
+use std::sync::Arc;
+
+use neo4rs::*;
+
+pub struct Neo4j {
+    pub graph: Arc<Graph>,
+}
+
+impl Neo4j {
+    pub async fn new(uri: &str, user: &str, password: &str) -> Neo4j {
+        let graph = Graph::new(uri, user, password).await.unwrap();
+
+        Neo4j {
+            graph: Arc::new(graph),
+        }
+    }
+
+    pub fn get_graph(&self) -> Arc<Graph> {
+        Arc::clone(&self.graph)
+    }
+}
