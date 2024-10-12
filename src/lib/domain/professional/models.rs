275a826c37be167263f24ade981ab09e67ae4044
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-10-12 02:46:14 +0200

    feat: Add Name and Email models to Professional struct
    This commit modifies the `Professional` struct in the `models.rs` file by adding the `Name` and `Email` fields. It also introduces the `Name` and `Email` models with their associated error types.
    
    The purpose of this commit is to enhance the `Professional` struct by including the necessary fields for name and email, and providing models to handle validation and error handling for these fields.
    
    Note: This commit message follows the established convention of using a prefix to indicate the type of change (feat for feature, fix for bug fix, chore for maintenance tasks) and provides a clear and concise description of the changes made.
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

--- src/lib/domain/professional/models.rs
+++ src/lib/domain/professional/models.rs
@@ -2,15 +2,15 @@ use thiserror::Error;
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Professional {
-  pub id: uuid::Uuid,
-  pub name: Name,
-  pub email: Email,
+    pub id: uuid::Uuid,
+    pub name: Name,
+    pub email: Email,
 }
 
 #[derive(Debug, Error)]
 pub enum ProfessionalError {
-  #[error("Error creating professional: {0}")]
-  CreateError(String),
+    #[error("Error creating professional: {0}")]
+    CreateError(String),
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
@@ -19,21 +19,24 @@ pub struct Name(String);
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub struct Email(String);
 
-
 #[derive(Clone, Debug, Error)]
 #[error("Name cannot be empty")]
 pub struct NameEmptyError;
 
 impl Name {
-  pub fn new(value: &str) -> Result<Name, NameEmptyError> {
-    let trimmed = value.trim();
+    pub fn new(value: &str) -> Result<Name, NameEmptyError> {
+        let trimmed = value.trim();
 
-    if trimmed.is_empty() {
-        Err(NameEmptyError)
-    } else {
-        Ok(Self(trimmed.to_string()))
+        if trimmed.is_empty() {
+            Err(NameEmptyError)
+        } else {
+            Ok(Self(trimmed.to_string()))
+        }
     }
-  }
+
+    pub fn as_str(&self) -> &str {
+        &self.0
+    }
 }
 
 #[derive(Clone, Debug, Error)]
@@ -41,13 +44,17 @@ impl Name {
 pub struct EmailEmptyError;
 
 impl Email {
-  pub fn new(value: &str) -> Result<Email, EmailEmptyError> {
-    let trimmed = value.trim();
+    pub fn new(value: &str) -> Result<Email, EmailEmptyError> {
+        let trimmed = value.trim();
 
-    if trimmed.is_empty() {
-        Err(EmailEmptyError)
-    } else {
-        Ok(Self(trimmed.to_string()))
+        if trimmed.is_empty() {
+            Err(EmailEmptyError)
+        } else {
+            Ok(Self(trimmed.to_string()))
+        }
     }
-  }
+
+    pub fn as_str(&self) -> &str {
+        &self.0
+    }
 }
