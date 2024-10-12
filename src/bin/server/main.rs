789fc277032bae084e6bd76d730a4cb24dc28965
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-10-12 02:47:36 +0200

    feat: Add Neo4j professional repository module
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -1,3 +1,19 @@
-fn main() {
+use std::sync::Arc;
+
+use anyhow::Ok;
+use ferriscare::{domain::professional::ports::ProfessionalRepository, infrastructure::{db::neo4j::Neo4j, professional::neo4j::professional_repository::Neo4jProfessionalRepository}};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
     println!("Hello, world!");
+    
+    let database = Neo4j::new("0.0.0.0:7687", "neo4j", "password").await;
+    let neo4j = Arc::new(database);
+
+    let professional_repository = Neo4jProfessionalRepository::new(Arc::clone(&neo4j));
+
+    let _ = professional_repository.create("Nathael", "nathael@bonnal.cloud").await;
+    let _ = professional_repository.create("Martin", "martin@matin.io").await;
+
+    Ok(())
 }
