9041d926cca718dafdbf70c68579633625980ad8
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-10-12 04:26:50 +0200

    feat: Add HTTP server module with router and listener
    This commit adds the `HttpServer` struct with its associated methods and dependencies. It includes a router for handling API routes and a TCP listener for accepting incoming connections. The `new` method initializes the server with a given configuration, and the `run` method starts the server and listens for incoming requests.
    
    The purpose of this commit is to implement the HTTP server functionality, allowing the application to handle API requests.
    
    Note: This commit message follows the established convention of using a prefix to indicate the type of change (feat for feature, fix for bug fix, chore for maintenance tasks) and provides a clear and concise description of the changes made.
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

---
+++ src/lib/application/http/handlers.rs
@@ -0,0 +1,66 @@
+use axum::{http::StatusCode, response::IntoResponse, Json};
+use serde::Serialize;
+
+pub struct ApiSuccess<T: Serialize + PartialEq>(StatusCode, Json<ApiResponseBody<T>>);
+
+impl<T> PartialEq for ApiSuccess<T>
+where
+    T: Serialize + PartialEq,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.0 == other.0 && self.1 .0 == other.1 .0
+    }
+}
+
+impl<T: Serialize + PartialEq> ApiSuccess<T> {
+    fn new(status: StatusCode, data: T) -> Self {
+        ApiSuccess(status, Json(ApiResponseBody::new(status, data)))
+    }
+}
+
+impl<T: Serialize + PartialEq> IntoResponse for ApiSuccess<T> {
+    fn into_response(self) -> axum::response::Response {
+        (self.0, self.1).into_response()
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum ApiError {
+    InternalServerError(String),
+    UnProcessableEntity(String),
+}
+
+impl From<anyhow::Error> for ApiError {
+    fn from(e: anyhow::Error) -> Self {
+        Self::InternalServerError(e.to_string())
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize)]
+pub struct ApiResponseBody<T: Serialize + PartialEq> {
+    status_code: u16,
+    data: T,
+}
+
+impl<T: Serialize + PartialEq> ApiResponseBody<T> {
+    pub fn new(status_code: StatusCode, data: T) -> Self {
+        Self {
+            status_code: status_code.as_u16(),
+            data,
+        }
+    }
+}
+
+impl ApiResponseBody<ApiErrorData> {
+    pub fn new_error(status_code: StatusCode, message: String) -> Self {
+        Self {
+            status_code: status_code.as_u16(),
+            data: ApiErrorData { message },
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Serialize)]
+pub struct ApiErrorData {
+    pub message: String,
+}
