338913a5bc0efa0509075da8eb2e7ec9f2555553
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-10-14 00:57:20 +0200

    feat: Add create_professional handler module
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

--- src/lib/application/http/handlers/create_professional.rs
+++ src/lib/application/http/handlers/create_professional.rs
@@ -1,11 +1,11 @@
 use std::sync::Arc;
 
-use axum::{Extension, Json};
+use axum::{http::StatusCode, Extension, Json};
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 
 use crate::domain::professional::{
-    models::{EmailEmptyError, NameEmptyError},
+    models::{EmailEmptyError, NameEmptyError, Professional, ProfessionalError},
     ports::ProfessionalService,
 };
 
@@ -19,15 +19,58 @@ enum ParseCreateProfessionalError {
     EmailEmpty(#[from] EmailEmptyError),
 }
 
+impl From<ParseCreateProfessionalError> for ApiError {
+    fn from(e: ParseCreateProfessionalError) -> Self {
+        let message = match e {
+            ParseCreateProfessionalError::EmailEmpty(e) => e.to_string(),
+            ParseCreateProfessionalError::NameEmpty(e) => e.to_string(),
+        };
+
+        Self::UnProcessableEntity(message)
+    }
+}
+
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord)]
-pub struct CreateProfessionalResponseData {}
+pub struct CreateProfessionalResponseData {
+  id: String,
+  name: String,
+  email: String,
+}
+
+impl From<&Professional> for CreateProfessionalResponseData {
+    fn from(professional: &Professional) -> Self {
+        CreateProfessionalResponseData {
+          id: professional.id.to_string(),
+          name: professional.name.as_str().to_string(),
+          email: professional.email.as_str().to_string(),
+        }
+    }
+}
+
+impl From<ProfessionalError> for ApiError {
+    fn from(e: ProfessionalError) -> Self {
+        match e {
+            ProfessionalError::CreateError(e) => Self::InternalServerError(e),
+        }
+    }
+}
 
 #[derive(Debug, Clone, PartialEq, Eq, Deserialize)]
-pub struct CreateProfessionalValidator {}
+pub struct CreateProfessionalValidator {
+    name: String,
+    email: String,
+}
 
-pub async fn create_professional<S: ProfessionalService>(
-    Extension(professional_service): Extension<Arc<S>>,
+pub async fn create_professional<P: ProfessionalService>(
+    Extension(professional_service): Extension<Arc<P>>,
     Json(body): Json<CreateProfessionalValidator>,
 ) -> Result<ApiSuccess<CreateProfessionalResponseData>, ApiError> {
-    todo!()
+    let name = body.name;
+    let email = body.email;
+
+    professional_service
+        .create(&name, &email)
+        .await
+        .map_err(ApiError::from)
+        .map(|ref professional| ApiSuccess::new(StatusCode::CREATED, professional.into()))
 }
