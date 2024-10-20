e4748154d8253b1958aa024dee3f9642b1c782a3
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-10-20 17:15:25 +0200

    feat: Add anstream, anstyle, and anstyle-parse dependencies
    This commit adds the `anstream`, `anstyle`, and `anstyle-parse` dependencies to the project. These dependencies are required for implementing the messaging functionality in the application.
    
    Note: This commit message follows the established convention of using a prefix to indicate the type of change (feat for feature, fix for bug fix, chore for maintenance tasks) and provides a clear and concise description of the changes made.
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

---
+++ src/lib/env.rs
@@ -0,0 +1,17 @@
+use clap::Parser;
+
+#[derive(Debug, Clone, Default, Parser)]
+pub struct Env {
+    #[clap(env)]
+    pub database_url: String,
+    #[clap(env)]
+    pub database_user: String,
+    #[clap(env)]
+    pub database_password: String,
+
+    #[clap(env)]
+    pub nats_url: String,
+
+    #[clap(env)]
+    pub port: String,
+}
