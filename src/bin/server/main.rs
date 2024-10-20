28de1a7fe8455c0288e75cc933d8f3fe0368d711
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-10-20 17:16:06 +0200

    feat: Update server main.rs to use environment variables for database and messaging configuration
    This commit updates the `server/main.rs` file to use environment variables for configuring the database and messaging settings. It introduces the `Env` struct and initializes it with the parsed environment variables. The database URL, username, and password are now retrieved from the `Env` struct when creating a new `Neo4j` instance. Additionally, the messaging URL is retrieved from the `Env` struct when creating a new `NatsMessaging` instance.
    
    Note: This commit message follows the established convention of using a prefix to indicate the type of change (feat for feature, fix for bug fix, chore for maintenance tasks) and provides a clear and concise description of the changes made.
    
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

--- src/bin/server/main.rs
+++ src/bin/server/main.rs
@@ -1,10 +1,13 @@
 use std::sync::Arc;
 
+use clap::Parser;
 use ferriscare::{
     application::http::{HttpServer, HttpServerConfig},
     domain::professional::services::ProfessionalServiceImpl,
+    env::Env,
     infrastructure::{
-        db::neo4j::Neo4j, professional::neo4j::professional_repository::Neo4jProfessionalRepository,
+        db::neo4j::Neo4j, messaging::nats::NatsMessaging,
+        professional::neo4j::professional_repository::Neo4jProfessionalRepository,
     },
 };
 
@@ -13,10 +16,29 @@ async fn main() -> anyhow::Result<()> {
     dotenv::dotenv().ok();
     tracing_subscriber::fmt::init();
 
-    let database = Neo4j::new("0.0.0.0:7687", "neo4j", "password").await;
+    let env = Arc::new(Env::parse());
+
+    let database = Neo4j::new(
+        &env.database_url,
+        &env.database_user,
+        &env.database_password,
+    )
+    .await;
     let neo4j = Arc::new(database);
 
-    let server_config = HttpServerConfig { port: "3333" };
+    let _messaging = Arc::new(NatsMessaging::new(&env.nats_url).await?);
+
+    // tokio::spawn(async move {
+    //     messaging
+    //         .subscribre("test", |e: Message| {
+    //             println!("{:?}", e);
+    //             Ok(())
+    //         })
+    //         .await
+    //         .unwrap();
+    // });
+
+    let server_config = HttpServerConfig::new(env.port.clone());
 
     let professional_repository = Neo4jProfessionalRepository::new(Arc::clone(&neo4j)).await;
     let professional_service = ProfessionalServiceImpl::new(professional_repository);
