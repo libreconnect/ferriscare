2181e04d52e6be517a15b80f2c5fda9dbdd51bfa
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-10-23 23:15:37 +0200

    feat: Handle create relationship message in ProfessionalService
    This commit adds the necessary code in the ProfessionalService to handle the create relationship message. It retrieves the professional by ID, and based on the result, publishes a message indicating whether the professional was found or not. This change improves the messaging functionality in the application.
    
    Signed-off-by: nathaelbonnal <pro.nathaelbonnal@gmail.com>

--- src/lib/domain/professional/services.rs
+++ src/lib/domain/professional/services.rs
@@ -1,5 +1,6 @@
 use std::sync::Arc;
 
+use serde::{Deserialize, Serialize};
 use tracing::info;
 
 use crate::application::ports::messaging_ports::MessagingPort;
@@ -33,6 +34,13 @@ where
     }
 }
 
+#[derive(Debug, Deserialize, PartialEq, Eq, PartialOrd, Ord, Serialize)]
+struct Message {
+    message: String,
+    professional_id: String,
+    company_id: String,
+}
+
 impl<P, M> ProfessionalService for ProfessionalServiceImpl<P, M>
 where
     P: ProfessionalRepository,
@@ -46,11 +54,56 @@ where
         self.professional_repository.find_by_id(id).await
     }
 
+    // @TODO: remove the unwrap in serde_json::to_string and handle the error
+    // @TOOD: move the struct Message in domain
     async fn handle_create_relationship(
         &self,
         message: ProfessionalAddRequestMessage,
     ) -> Result<(), ProfessionalError> {
         info!("Handling create relationship message: {:?}", message);
+
+        let id = message.professional_id;
+        let professional = self.find_by_id(id.clone()).await;
+
+        match professional {
+            Ok(professional) => {
+                info!("Professional found: {:?}", professional);
+
+                let msg = Message {
+                    message: format!("Professional {} found", &id),
+                    professional_id: professional.id.to_string(),
+                    company_id: message.company_id,
+                };
+
+                self.messaging
+                    .publish_message(
+                        "professional.add.validated".to_string(),
+                        serde_json::to_string(&msg).unwrap(),
+                    )
+                    .await
+                    .map_err(|_| {
+                        ProfessionalError::NotFound(format!("Professional {} not found", &id))
+                    })?;
+            }
+            Err(e) => {
+                info!("Error finding professional: {:?}", e);
+
+                let msg = Message {
+                    message: format!("Professional {} not found", &id),
+                    professional_id: id.clone(),
+                    company_id: message.company_id,
+                };
+
+                let msg_str = serde_json::to_string(&msg).unwrap();
+
+                self.messaging
+                    .publish_message("professional.add.failure".to_string(), msg_str)
+                    .await
+                    .map_err(|_| {
+                        ProfessionalError::NotFound(format!("Professional {} not found", &id))
+                    })?;
+            }
+        }
 
         Ok(())
     }
